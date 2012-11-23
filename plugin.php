42c021f8cf66359b9a9629daa89128c1a5e1715e
moon_watch <moon_watch@be9c13ff-3d72-400f-8046-48c2d9f2adff>
2012-11-23 16:33:42 +0000

    More settings added. Initial custom post types creation code added.

--- plugin.php
+++ plugin.php
@@ -32,19 +32,14 @@ require_once dirname( __FILE__ ) . '/flags_data.php';
 * 
 */
 class Multilingual_WP {
-	
-	public function plugin_init() {
-		// Creating a custom table
-		/*new scbTable( 'example_table', __FILE__, "
-			example_id int(20),
-			example varchar(100),
-			PRIMARY KEY  (example_id)
-		");*/
+	public static $options;
+	private $pt_prefix = 'mlwp_';
 
+	public function plugin_init() {
 		$plugin_url = plugins_url( dirname( __FILE__ ) );
 
 		// Creating an options object
-		$options = new scbOptions( 'mlwp_options', __FILE__, array(
+		self::$options = new scbOptions( 'mlwp_options', __FILE__, array(
 			'languages' => array(
 				'en' => array(
 					'locale' => 'en_US',
@@ -63,20 +58,88 @@ class Multilingual_WP {
 					'time_format' => '',
 				)
 			),
+			'default_lang' => false,
 			'enabled_langs' => array(  ),
 			'dfs' => '24',
+			'enabled_pt' => array( 'post', 'page' ),
+			'generated_pt' => array(),
+			'show_ui' => false,
 		) );
 
 		// Creating settings page objects
 		if ( is_admin() ) {
 			require_once( dirname( __FILE__ ) . '/settings-page.php' );
-			new Example_Admin_Page( __FILE__, $options );
+			new Multilingual_WP_Admin_Page( __FILE__, self::$options );
 		}
 	}
 
 	function __construct() {
-		# code...
+		add_action( 'init', array( $this, 'init' ), 100 );
+	}
+
+	public function init() {
+		$this->register_post_types();
+	}
+
+	private function register_post_types() {
+		$enabled_pt = self::$options->enabled_pt;
+
+		$generated_pt = array();
+
+		if ( $enabled_pt ) {
+			$enabled_langs = self::$options->enabled_langs;
+			if ( ! $enabled_langs ) {
+				return false;
+			}
+
+			$post_types = get_post_types( array(  ), 'objects' );
+
+			$languages = self::$options->languages;
+			$show_ui = (bool) self::$options->show_ui;
+			// var_dump($show_ui);
+			// var_dump($post_types);
+
+			foreach ( $enabled_pt as $pt_name ) {
+				$pt = isset( $post_types[$pt_name] ) ? $post_types[$pt_name] : false;
+				if ( ! $pt ) {
+					continue;
+				}
+				foreach ($enabled_langs as $lang) {
+					$name = "{$this->pt_prefix}{$pt_name}_{$lang}";
+					$labels = array_merge(
+						(array) $pt->labels,
+						array( 'menu_name' => $pt->labels->menu_name . ' - ' . $languages[ $lang ]['label'], )
+					);
+					$args = array(
+						'labels' => $labels,
+						'public' => true,
+						'exclude_from_search' => true,
+						'show_ui' => $show_ui, 
+						'query_var' => false,
+						'rewrite' => true,
+						'capability_type' => $pt->capability_type,
+						'capabilities' => (array) $pt->cap,
+						'hierarchical' => $pt->hierarchical,
+						'menu_position' => 9999,
+						'has_archive' => $pt->has_archive,
+						'supports' => isset( $pt->supports ) ? $pt->supports : array(),
+						'can_export' => $pt->can_export,
+					);
+					$result = register_post_type($name, $args);
+					if ( ! is_wp_error( $result ) ) {
+						$generated_pt[] = $name;
+					}
+				}
+
+			}
+		}
+
+		// Update the option
+		self::$options->generated_pt = $generated_pt;
 	}
 }
 
 scb_init( array( 'Multilingual_WP', 'plugin_init' ) );
+
+global $Multilingual_WP;
+$Multilingual_WP = new Multilingual_WP();
--- settings-page.php
+++ settings-page.php
@@ -1,6 +1,41 @@
 <?php
 
-class Example_Admin_Page extends scbAdminPage {
+class Multilingual_WP_Admin_Page extends scbAdminPage {
+	protected $textdomain = 'multilingual-wp';
+	protected $admin_notice = false;
+
+	function _page_content_hook() {
+		if ( $this->admin_notice ) {
+			$this->admin_msg( $this->admin_notice );
+		}
+
+		$this->page_header();
+		$this->page_content();
+		$this->page_footer();
+	}
+
+	// Manually handle option saving ( use Settings API instead )
+	function form_handler() {
+		if ( empty( $_POST['action'] ) )
+			return false;
+
+		check_admin_referer( $this->nonce );
+
+		if ( !isset($this->options) ) {
+			trigger_error('options handler not set', E_USER_WARNING);
+			return false;
+		}
+
+		$new_data = wp_array_slice_assoc( $_POST, array_keys( $this->options->get_defaults() ) );
+
+		$new_data = stripslashes_deep( $new_data );
+
+		$new_data = $this->validate( $new_data, $this->options->get() );
+
+		$this->options->set( $new_data );
+
+		$this->admin_notice = __( 'Settings <strong>saved</strong>.', 'multilingual-wp' );
+	}
 
 	function setup() {
 		$this->args = array(
@@ -8,8 +43,10 @@ class Example_Admin_Page extends scbAdminPage {
 		);
 
 		$this->plugin_url = plugins_url( dirname( __FILE__ ) );
-		// var_dump($this);
-		add_action('admin_enqueue_scripts', array( $this, 'enqueue_scripts' ), 10);
+
+		add_action( 'admin_enqueue_scripts', array( $this, 'enqueue_scripts' ), 10 );
+
+		add_action( 'init', array( $this, 'form_handler' ) );
 	}
 
 	public function enqueue_scripts( $handle ) {
@@ -28,90 +65,16 @@ class Example_Admin_Page extends scbAdminPage {
 	}
 
 	function page_content() {
-		$plugin_url = plugins_url( '', __FILE__ );
+		$languages = $this->options->languages;
 
+		// We want to put all of the output in a single <form>
 		ob_start();
 
-		echo '<div class="js-tab" id="tab_general" title="' . __( 'General Settings', 'multilingual-wp' ) . '">';
-		apply_filters( 'the_content', '' );
+		// Render the General settings tab
+		$this->general_settings_tab( $languages );
 
-		echo html( 'h3', 'Enabled Languages' );
-
-		$default_settings = $l_opts = array();
-		$enabled_langs = $this->options->get( 'enabled_langs' );
-		$languages = $this->options->get( 'languages' );
-		foreach ( $languages as $lang => $data ) {
-			$l_opts[$lang] = '<img style="margin-bottom:-8px;padding:0 5px;" src="' . $plugin_url . '/flags/24/' . $data['icon'] . '" alt="' . esc_attr( $data['label'] ) . '" /> ' . $data['label'] . '<br />';
-		}
-		$default_settings[] = array(
-			'title' => __( 'Please select the languages that you want your website to support.', 'multilingual-wp' ),
-			'type' => 'checkbox',
-			'name' => "enabled_langs",
-			'checked' => $enabled_langs,
-			'choices' => $l_opts
-		);
-
-		$dfs = $this->options->get( 'dfs' );
-
-		$default_settings[] = array(
-			'title' => __( 'Default Flag Size', 'multilingual-wp' ),
-			'type' => 'select',
-			'name' => "dfs",
-			'desc' => sprintf( __( 'Set the default size of the flags used to represent each language(usually in language-select widgets). You can override this on a per-widget bassis. Here is an example of the selected size: <br />%s', 'multilingual-wp' ), '<img style="margin-bottom:-8px;padding:0 5px;" src="' . $plugin_url . '/flags/' . intval( $dfs ) . '/antarctica.png" alt="' . __( 'Antarctica', 'multilingual-wp' ) . '" />' ),
-			'value' => $dfs,
-			'choices' => array( '16' => '16 x 16', '24' => '24 x 24', '32' => '32 x 32', '48' => '48 x 48', '64' => '64 x 64' ),
-			'extra' => array( 'id' => 'flag_size_select' )
-		);
-
-		echo $this->table( $default_settings );
-
-		echo '</div> <!-- Tab end -->';
-
-		echo '<div class="js-tab" id="tab_languages" title="' . __( 'Language Settings', 'multilingual-wp' ) . '">';
-		apply_filters( 'the_content', __( 'Here you can change the settings for each supported language.', 'multilingual-wp' ) );
-
-		foreach ($languages as $lang => $data) {
-			$this->start_box( $data['label'] );
-			// echo html( 'h3', $data['label'] );
-
-			echo $this->table( array(
-				array(
-					'title' => __( 'Language Label', 'multilingual-wp' ),
-					'type' => 'text',
-					'name' => "languages[$lang][label]",
-					'desc' => __( 'Enter the label that will be used to represent this language. This will be used in the admin interface, language selector widget, etc.', 'multilingual-wp' ),
-					'value' => $data['label']
-				),
-				array(
-					'title' => __( 'Language Locale', 'multilingual-wp' ),
-					'type' => 'text',
-					'name' => "languages[$lang][locale]",
-					'desc' => __( 'Enter the PHP/WordPress locale for this language. For instance: <code>en_US</code>.', 'multilingual-wp' ),
-					'value' => $data['locale']
-				),array(
-					'title' => __( 'Language Flag', 'multilingual-wp' ),
-					'type' => 'text',
-					'name' => "languages[$lang][icon]",
-					'desc' => __( 'Select the flag that will represent this language. The current flag is <img src="' . $this->plugin_url . 'flags/24/' . $data['icon'] . '" class="lang_icon" alt="" />', 'multilingual-wp' ),
-					'value' => $data['icon'],
-					'extra' => array( 'class' => 'regular-text mlwp_flag_input' )
-					// 'render' => array( $this, 'render_lf_dd' )
-				)
-			) );
-
-			$this->end_box();
-
-			// 'en' => array(
-			// 		'locale' => 'en_US',
-			// 		'label' => 'English',
-			// 		'icon' => 'united-states.png',
-			// 		'na_message' => 'Sorry, but this article is not available in English.',
-			// 		'date_format' => '',
-			// 		'time_format' => '',
-			// 	),
-		}
-
-		echo '</div> <!-- Tab end -->';
+		// Render the Languages tab
+		$this->languages_tab( $languages );
 
 		echo $this->form_wrap( ob_get_clean() );
 	}
@@ -139,10 +102,120 @@ class Example_Admin_Page extends scbAdminPage {
 			</div>';
 	}
 
-	public function render_lf_dd( $value, $field ) {
-		$value = $value ? $value : $field->value;
-		var_dump($value, $field);
-		return 'lol';
+	private function general_settings_tab( $languages ) {
+		echo '<div class="js-tab" id="tab_general" title="' . __( 'General Settings', 'multilingual-wp' ) . '">';
+
+		echo html( 'h3', 'Enabled Languages' );
+
+		$default_settings = $l_opts = array();
+		$enabled_langs = $this->options->enabled_langs;
+		$default_lang = $this->options->default_lang;
+		foreach ( $languages as $lang => $data ) {
+			$l_opts[$lang] = '<img style="margin-bottom:-8px;padding:0 5px;" src="' . $this->plugin_url . '/flags/24/' . $data['icon'] . '" alt="' . esc_attr( $data['label'] ) . '" /> ' . $data['label'] . '<br />';
+		}
+
+		$default_settings[] = array(
+			'title' => __( 'Default Language', 'multilingual-wp' ),
+			'type' => 'select',
+			'name' => "default_lang",
+			'desc' => __( 'Please select your blog\'s default language.', 'multilingual-wp' ),
+			'value' => $default_lang,
+			'choices' => array_map( 'strip_tags', $l_opts ),
+			'extra' => array( 'id' => 'default_lang' )
+		);
+		
+		$default_settings[] = array(
+			'title' => __( 'Please select the languages that you want your website to support.', 'multilingual-wp' ),
+			'type' => 'checkbox',
+			'name' => "enabled_langs",
+			'checked' => $enabled_langs,
+			'choices' => $l_opts
+		);
+
+		$dfs = $this->options->dfs;
+
+		$default_settings[] = array(
+			'title' => __( 'Default Flag Size', 'multilingual-wp' ),
+			'type' => 'select',
+			'name' => "dfs",
+			'desc' => sprintf( __( 'Set the default size of the flags used to represent each language(usually in language-select widgets). You can override this on a per-widget bassis. Here is an example of the selected size: <br />%s', 'multilingual-wp' ), '<img style="margin-bottom:-8px;padding:0 5px;" src="' . $this->plugin_url . '/flags/' . intval( $dfs ) . '/antarctica.png" alt="' . __( 'Antarctica', 'multilingual-wp' ) . '" />' ),
+			'value' => $dfs,
+			'choices' => array( '16' => '16 x 16', '24' => '24 x 24', '32' => '32 x 32', '48' => '48 x 48', '64' => '64 x 64' ),
+			'extra' => array( 'id' => 'flag_size_select' )
+		);
+
+		$pts_opts = array( 'post' => __( 'Post' ) . '<br />', 'page' => __( 'Page' ) . '<br />' );
+		$post_types = get_post_types( array( 'show_ui' => true, '_builtin' => false ), 'objects' );
+		if ( $post_types ) {
+			foreach ($post_types as $pt => $data) {
+				if ( in_array( $pt, $this->options->generated_pt ) ) {
+					continue;
+				}
+				$pts_opts[ $pt ] = $data->labels->name . '<br />';
+			}
+		}
+		$enabled_pt = $this->options->enabled_pt;
+
+		$default_settings[] = array(
+			'title' => __( 'Please select which post types you want to be multilingual.', 'multilingual-wp' ),
+			'type' => 'checkbox',
+			'name' => "enabled_pt",
+			'checked' => $enabled_langs,
+			'choices' => $pts_opts
+		);
+
+		// var_dump($this->options->show_ui);
+
+		$default_settings[] = array(
+			'title' => __( 'Show UI?', 'multilingual-wp' ),
+			'type' => 'select',
+			'name' => "show_ui",
+			'value' => $this->options->show_ui ? true : false,
+			'choices' => array( '' => __( 'No', 'multilingual-wp' ), '1' => __( 'Yes', 'multilingual-wp' ) ),
+			'desc' => __( 'Whether to display the User Interface for the post types added by Multilingual WP.', 'multilingual-wp' )
+		);
+
+		echo $this->table( $default_settings );
+
+		echo '</div> <!-- Tab end -->';
+	}
+
+	private function languages_tab( $languages ) {
+		echo '<div class="js-tab" id="tab_languages" title="' . __( 'Language Settings', 'multilingual-wp' ) . '">';
+		apply_filters( 'the_content', __( 'Here you can change the settings for each supported language.', 'multilingual-wp' ) );
+
+		foreach ($languages as $lang => $data) {
+			$this->start_box( $data['label'] );
+
+			echo $this->table( array(
+				array(
+					'title' => __( 'Language Label', 'multilingual-wp' ),
+					'type' => 'text',
+					'name' => "languages[$lang][label]",
+					'desc' => __( 'Enter the label that will be used to represent this language. This will be used in the admin interface, language selector widget, etc.', 'multilingual-wp' ),
+					'value' => $data['label']
+				),
+				array(
+					'title' => __( 'Language Locale', 'multilingual-wp' ),
+					'type' => 'text',
+					'name' => "languages[$lang][locale]",
+					'desc' => __( 'Enter the PHP/WordPress locale for this language. For instance: <code>en_US</code>.', 'multilingual-wp' ),
+					'value' => $data['locale']
+				),array(
+					'title' => __( 'Language Flag', 'multilingual-wp' ),
+					'type' => 'text',
+					'name' => "languages[$lang][icon]",
+					'desc' => __( 'Select the flag that will represent this language. The current flag is <img src="' . $this->plugin_url . 'flags/24/' . $data['icon'] . '" class="lang_icon" alt="" />', 'multilingual-wp' ),
+					'value' => $data['icon'],
+					'extra' => array( 'class' => 'regular-text mlwp_flag_input' )
+					// 'render' => array( $this, 'render_lf_dd' )
+				)
+			) );
+
+			$this->end_box();
+		}
+
+		echo '</div> <!-- Tab end -->';
 	}
 
 	function page_footer() {
@@ -172,38 +245,3 @@ class Example_Admin_Page extends scbAdminPage {
 		parent::page_footer();
 	}
 }
-
-
-class Example_Boxes_Page extends scbBoxesPage {
-
-	function setup() {
-		$this->args = array(
-			'page_title' => 'scb Example Boxes',
-			'columns' => 4
-		);
-
-		$this->boxes = array(
-			array( 'settings', 'Settings Box', 'normal' ),
-			array( 'right', 'Right Box', 'side' ),
-			array( 'third', 'Third Box', 'column3' ),
-			array( 'fourth', 'Fourth Box', 'column4' ),
-		);
-	}
-
-	function settings_box() {
-		echo html( 'p', 'This is a settings box.' );
-	}
-
-	function right_box() {
-		echo html( 'p', 'This is a box on the right.' );
-	}
-
-	function third_box() {
-		echo html( 'p', 'This is a box in the third column.' );
-	}
-
-	function fourth_box() {
-		echo html( 'p', 'This is a box in the fourth column.' );
-	}
-}
-
